9b0c6f3416d60a76d17ca805a772d81b93a8bbce
Celdama <celdama.dev@gmail.com>
2022-09-02 08:17:53 +0200

    screens: add boilerplate

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text } from 'react-native';
+
+const SignupScreen = () => {
+  return <Text style={{ fontSize: 48 }}>SignupScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
---
+++ src/screens/TrackCreateScreen.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text } from 'react-native';
+
+const TrackCreateScreen = () => {
+  return <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackCreateScreen;
