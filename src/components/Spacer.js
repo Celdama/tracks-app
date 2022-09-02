21d0015bdda727047e2db193c3e34ee652b2054e
Celdama <celdama.dev@gmail.com>
2022-09-02 09:38:53 +0200

    components/Spacer: create

---
+++ src/components/Spacer.js
@@ -0,0 +1,9 @@
+import { StyleSheet, View } from 'react-native';
+
+const Spacer = ({ children }) => {
+  return <View>{children}</View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default Spacer;
