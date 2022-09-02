6948ff68b8443ab14a71223f0e015768f0589094
Celdama <celdama.dev@gmail.com>
2022-09-02 09:39:56 +0200

    screens/SignupScreen: use Spacer component

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,16 @@
 import { StyleSheet } from 'react-native';
 import { Button, Input, Text } from 'react-native-elements';
 
+import Spacer from '../components/Spacer';
+
 const SignupScreen = ({ navigation }) => {
   return (
-    <>
+    <Spacer>
       <Text h3>Sign Up for Tracker</Text>
       <Input label='Email' />
       <Input label='Password' />
       <Button title='Sign Up' />
-    </>
+    </Spacer>
   );
 };
 
