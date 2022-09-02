333c6f2bb627e8dad5404911c858ea01dd44bf8e
Celdama <celdama.dev@gmail.com>
2022-09-02 08:50:11 +0200

    screens: Testing the Navigation Flow

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,7 +1,19 @@
-import { StyleSheet, Text } from 'react-native';
+import { Button, StyleSheet, Text } from 'react-native';
 
-const SignupScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SignupScreen</Text>;
+const SignupScreen = ({ navigation }) => {
+  return (
+    <>
+      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+      <Button
+        title='Go to signin'
+        onPress={() => navigation.navigate('Signin')}
+      />
+      <Button
+        title='Go to mainFlow'
+        onPress={() => navigation.navigate('mainFlow')}
+      />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,7 +1,17 @@
-import { StyleSheet, Text } from 'react-native';
+import { Button, StyleSheet, Text } from 'react-native';
 
-const AccountScreen = () => {
-  return <Text style={{ fontSize: 48 }}>AccountScreen</Text>;
+const AccountScreen = ({ navigation }) => {
+  return (
+    <>
+      <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+      <Button
+        title='Go to track detail'
+        onPress={() => {
+          navigation.navigate('TrackDetail');
+        }}
+      />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
